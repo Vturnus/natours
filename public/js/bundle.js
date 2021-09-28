5fa453152266a3a04b373c402d5ea3b529791c42
William <amir.korkinejad@yahoo.com>
2021-09-28 14:39:02 +0330

    added signup page

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9094,7 +9094,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7113" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "6250" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
