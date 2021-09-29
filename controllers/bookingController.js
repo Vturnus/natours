1c0c72b6bcba420ddd3efc738cc0e561a8b7e21a
William <amir.korkinejad@yahoo.com>
2021-09-30 01:51:27 +0330

    changing var

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -53,7 +53,7 @@ const createBookingCheckout = async session => {
     const user = (await User.findOne({
         email: session.customer_email
     })).id;
-    const price = session.line_items[0].amount / 100;
+    const price = session.amount_total / 100;
     await Booking.create({
         tour,
         user,
